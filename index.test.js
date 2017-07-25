180f710d1becf1df1035f7b292b767de41bf3a53
meme <meme@users.noreply.github.com>
2017-07-25 15:46:42 -0400

    Use environment

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
 const request = require('supertest')
 const index = require('./index')
 
-test('response hello world', () => request(index).get('/').expect(200, 'Hello World!'))
+test('response hello world', () => request(index).get('/').expect(200, 'Hello from Elastic Beanstalk!'))
